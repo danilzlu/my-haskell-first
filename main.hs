7f9bb3278ccfb681b02cfe6c53a726ef7493c50a
danilzlu <37219798+danilzlu@users.noreply.github.com>
2022-04-08 16:54:18 +0300

    Lesson 4 added

--- main.hs
+++ main.hs
@@ -2,10 +2,17 @@
 import Data.List
 
 compareLastNames name1 name2 = compare lastName1 lastName2
-  where lastName1 = snd name1
-        lastName2 = snd name2
+    where lastName1 = snd name1
+          lastName2 = snd name2
+
+f x = x * 2
+
+g y = y + 2
 
 names = [("Иэн","Кертис"), ("Бернард","Самнер"), ("Питер","Хук"),("Стивен","Моррис")]
 
 main = do
     putStrLn "Hello!"
+
+fac 0 = 1
+fac n = n * fac (n-1)
